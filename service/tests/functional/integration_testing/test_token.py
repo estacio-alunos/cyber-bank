4d5fb72926b888b0a123de03f88c229154779098
Carlos Eduardo <eduardo191949@hotmail.com>
2023-11-23 22:02:20 -0300

    test(test-token): Adicionado novos caso de uso - Teste para usuario inexistente - teste para obter access e refresh token - renomeado os nomes dos metodos para refletir melhor a proposta do test -

--- service/tests/functional/integration_testing/test_token.py
+++ service/tests/functional/integration_testing/test_token.py
@@ -3,20 +3,29 @@ import pytest
 from rest_framework.test import APIClient
 from django.contrib.auth.models import User
 
+@pytest.fixture
+def client() :    
+    return APIClient()
 
 @pytest.fixture
-def user_default():
-    user = User.objects.create_user(username='admin', password='carlos')
-    return user
-
+def auth():
+    return User.objects.create_user(username='admin', password='carlos')
 
 @pytest.mark.django_db
-def test_create_cep(user_default):
-    client = APIClient()
+def test_retorna_acesso_negado_para_usuario_inexistent(client):
+    response = client.post('/api/token/', data={
+        'username': 'usuario_inexistent',
+        'password': 'senha',
+    })
 
+    assert response.status_code == 401
+
+@pytest.mark.django_db
+def test_verifica_a_existencia_de_access_e_refresh_token(client, auth):
     response = client.post('/api/token/', data={
         'username': 'admin',
         'password': 'carlos',
     })
 
-    assert response.status_code == 200
+    assert 'access' in response.data
+    assert 'refresh' in response.data
